ac0bb74c06971cd8031bf17df02f8c85af26205e
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-12-25 15:55:23 +0530

    Rename files

--- src/DatePicker.test.js
+++ src/DatePicker.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import 'jest-styled-components'
-import App from './App'
+import DatePicker from './DatePicker'
 
 it('renders without crashing', () => {
-  renderer.create(<App />).toJSON()
+  renderer.create(<DatePicker />).toJSON()
 })
