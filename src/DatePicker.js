b19998ca7e159325ef827c3a81fa3b82b03a337d
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-12-25 18:15:36 +0530

    :bug: Move to styled components & show the use of snapshot testing
    A snapshot of the component was saved before this commit, so any changes to the component now results in a diff which fails the tests until the new snaphshot file is overridden & committed to the repo. Check the travis builds to see the log of this particular step

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -1,21 +1,51 @@
-import React, { Component } from 'react'
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
 import logo from './logo.svg'
-import './DatePicker.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
+const Container = styled.div`
+  text-align: center;
+`
+
+const Header = styled.header`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`
+const logoSpin = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+}
+`
+
+const Img = styled.img`
+  animation: ${logoSpin} infinite 20s linear;
+  height: 80px;
+`
+const H1 = styled.h1`
+  font-size: 1.5em;
+`
+
+const Intro = styled.p`
+  font-size: large;
+`
+Intro.displayName = 'App-intro'
+
+const DatePicker = () => {
+  return (
+    <Container>
+      <Header>
+        <Img src={logo} alt="logo" />
+        <H1>Welcome to React</H1>
+      </Header>
+      <Intro>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </Intro>
+    </Container>
+  )
 }
 
-export default App
+export default DatePicker
